40cfac0c1e611e044123f8306e509126c10c2e9d
Rozarai <bomberdude@hotmail.com>
2020-01-18 19:30:25 +0800

    Meiling boss

---
+++ Assets/Scripts/MeiLingBoss.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeiLingBoss : MonoBehaviour
+{
+
+
+    enum MeiState
+    {
+        IDLE,
+        DMG,
+        DROP,
+        LASER,
+        BOUNCE,
+        DEAD,
+    }
+
+    MeiState currState;
+    MeiState prevState;
+
+    public bool active;
+    public float walkSpeed;
+    public float jumpForce;
+
+    public Rigidbody2D rigidbody2d;
+    public Vector3 movement;
+
+
+    public float wallKissThreshold;
+    public float jumpThreshold;
+    private int layermask;
+    public SpriteRenderer spriteRenderer;
+
+    public Transform laserPrefab;
+    public Transform bulletPrefab;
+
+    float bounceTime;
+    float idleTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rigidbody2d = this.GetComponent<Rigidbody2D>();
+
+        currState = MeiState.BOUNCE;
+        prevState = MeiState.LASER;
+        bounceTime = 5f;
+       // active = false;
+        layermask = (1 << 8); //Player
+        layermask |= (1 << 9);//enemy
+        layermask |= (1 << 10);//camera
+        layermask = ~layermask;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (currState)
+        {
+            case MeiState.IDLE:
+                Idle();
+                break;
+            case MeiState.DMG:
+                break;
+            case MeiState.DROP:
+                StartCoroutine(MikoBullets());
+                break;
+            case MeiState.LASER:
+             StartCoroutine(MikoLazers());
+                break;
+            case MeiState.BOUNCE:
+                BouncePhase();
+                break;
+            case MeiState.DEAD:
+                break;
+        }
+
+    }
+
+
+    void BouncePhase()
+    {
+        Movement();
+        Hopping();
+        if (bounceTime < 0.0f)
+        {
+            if(prevState == MeiState.LASER)
+            {
+                prevState = currState = MeiState.DROP;
+            }
+            else
+            {
+                prevState = currState = MeiState.LASER;
+            }
+
+            rigidbody2d.velocity = new Vector2 (0f,0f);
+            rigidbody2d.gravityScale = 0f;
+        }
+
+        bounceTime -= Time.deltaTime;
+    }
+
+    void Idle()
+    {
+        idleTime -= Time.deltaTime;
+        if(idleTime<0.0f)
+        {
+            currState = MeiState.BOUNCE;
+            rigidbody2d.gravityScale = 2.5f;
+            bounceTime = 5f;
+
+        }
+    }
+
+
+
+    void Movement()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.right, wallKissThreshold, layermask);
+        RaycastHit2D hit2 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.left, wallKissThreshold, layermask);
+
+        if (hit || hit2)
+        {
+            Debug.DrawLine(new Vector2(transform.position.x, transform.position.y), hit.point, Color.cyan);
+            Debug.DrawLine(new Vector2(transform.position.x, transform.position.y), hit2.point, Color.cyan);
+        }
+        if (hit)
+            spriteRenderer.flipX = true;
+        if (hit2)
+            spriteRenderer.flipX = false;
+
+
+
+        movement = rigidbody2d.velocity;
+
+        if (spriteRenderer.flipX)
+            movement.x = -walkSpeed;
+        else
+            movement.x = walkSpeed;
+        // Set player new position
+        rigidbody2d.velocity = movement;
+    }
+
+    void Hopping()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), Vector2.down, jumpThreshold, layermask);
+       
+        if(hit)
+        {
+            rigidbody2d.velocity = new Vector2(rigidbody2d.velocity.x, jumpForce);
+        }
+    }
+
+    IEnumerator MikoLazers()
+    {
+
+        currState = MeiState.IDLE;
+        idleTime = 6f;
+        yield return new WaitForSeconds(1.5f);
+        while (transform.position.y < 6.5f)
+        {
+            transform.position= Vector2.MoveTowards(transform.position, transform.position + new Vector3(0f, 6.5f, 0f), 1f*Time.deltaTime);
+            yield return null;
+
+        }
+
+        float i;
+        for(i = 0.0f; i>-360f; i-=18f)
+        {
+            Instantiate(laserPrefab, transform.position, Quaternion.Euler(0f, 0f, i));
+            Instantiate(laserPrefab, transform.position, Quaternion.Euler(0f, 0f, -i+180f));
+
+            yield return new WaitForSeconds(0.03f);
+        }
+
+        yield return 0;
+    }
+
+    IEnumerator MikoBullets()
+    {
+        currState = MeiState.IDLE;
+        idleTime = 6f;
+        yield return new WaitForSeconds(.5f);
+        while (transform.position.y < 5.5f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, transform.position + new Vector3(0f, 5.5f, 0f), 1f * Time.deltaTime);
+            yield return null;
+
+        }
+
+        int i;
+        for (i = 0; i < 20; ++i)
+        {
+
+            float ranX = Random.Range(-6f, 6f);
+            float ranY = Random.Range(4f, 6f);
+            Vector2 newpos = transform.position + new Vector3(ranX, ranY, 0f);
+            Instantiate(bulletPrefab, newpos, Quaternion.identity);
+
+            yield return new WaitForSeconds(0.25f);
+        }
+        yield return 0;
+
+    }
+}
---
+++ Assets/Scripts/MikoLaser.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MikoLaser : MonoBehaviour
+{
+    // Laser box collider
+    public BoxCollider2D boxCollider;
+
+    // Duration of laser life
+    public float laserDuration = 5.0f;
+
+    // Bool if laser is firing
+    bool laserFiring;
+
+    // Desired lasers final size
+    public float desiredWidth = 50.0f;
+    public float desiredLength = 10.0f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // Grab box collider
+        boxCollider = GetComponent<BoxCollider2D>();
+
+        // Set both collider and firing to false
+        boxCollider.enabled = false;
+        laserFiring = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // if Time is moving
+        if (Time.deltaTime > 0.0f)
+        {
+            // If laser has reached its end life
+            if (laserDuration < 0.0f)
+            {
+                // set firing to false
+                laserFiring = false;
+
+                // Start shrinking the laser
+                this.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - .1f, 1f);
+
+                // Once the laser shrinks into a thin beam, destroy the laser
+                if (this.transform.localScale.y < 0.0f)
+                    Destroy(this.gameObject);
+            }
+
+            // else if the laser have not reaches the correct width, grow its width
+            else if (transform.localScale.x < desiredWidth)
+                this.transform.localScale = new Vector3(transform.localScale.x + .55f, 1.0f, 1f);
+
+            // else if the laser have not reaches the correct length, grow its length
+            else if (transform.localScale.y < desiredLength)
+            {
+                this.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + .5f, 1f);
+
+                // On both laser collider and firing to be true
+                boxCollider.enabled = true;
+
+                laserFiring = true;
+            }
+
+            // While its firing, shrink and grow the laser for extra realistic effect and decrease its firing time
+            if (laserFiring)
+            {
+                this.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y - .1f, 1f);
+                laserDuration -= Time.deltaTime;
+            }
+        }
+
+
+    }
+}
